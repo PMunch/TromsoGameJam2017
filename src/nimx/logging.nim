53efd1d41f7a3b01fe3a13faf3e1ef397e62e984
Brecht Machiels <brecht@mos6581.org>
2016-06-24 00:11:41 +0200

    Fix building on nim 0.14.2
    Nim symbols need to be referred to in an emit pragma by enclosing them
    in backticks.

--- src/nimx/logging.nim
+++ src/nimx/logging.nim
@@ -10,7 +10,7 @@ when defined(macosx) or defined(ios):
     """.}
 
     proc NSLog_imported(a: cstring) =
-        {.emit: "NSLog(CFSTR(\"%s\"), a);" .}
+        {.emit: "NSLog(CFSTR(\"%s\"), `a`);" .}
 
     proc log*(a: varargs[string, `$`]) = NSLog_imported(a.join())
 elif defined(android):
@@ -19,7 +19,7 @@ elif defined(android):
     """.}
 
     proc droid_log_imported(a: cstring) =
-        {.emit: """__android_log_print(ANDROID_LOG_INFO, "NIM_APP", a);""".}
+        {.emit: """__android_log_print(ANDROID_LOG_INFO, "NIM_APP", `a`);""".}
     proc log*(a: varargs[string, `$`]) = droid_log_imported(a.join())
 else:
     proc log*(a: varargs[string, `$`]) = echo a
